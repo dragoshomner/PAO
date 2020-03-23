9ceede24e47fd5609d21d79ae4a6a817d024205e
Dragos Homner <dragos.homner@gmail.com>
2020-03-23 19:34:37 +0200

    tema 3 ex1

---
+++ Homework 3/tema 3/src/ex1.java
@@ -0,0 +1,49 @@
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ex1 {
+
+	public static boolean checkAnagrame(HashMap <Character, Integer> hm1,
+			HashMap <Character, Integer> hm2) {
+		
+		for (char letter = 'a'; letter < 'z'; letter++) {
+			int valHm1 = hm1.getOrDefault(letter, 0);
+			int valHm2 = hm2.getOrDefault(letter, 0);
+			
+			if (valHm1 != valHm2) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		HashMap<Character, Integer> lettersMap1 = new HashMap<Character, Integer>();
+		HashMap<Character, Integer> lettersMap2 = new HashMap<Character, Integer>();
+		StringBuilder word1, word2;
+		Scanner sc = new Scanner(System.in);
+		word1 = new StringBuilder(sc.nextLine());
+		word2 =  new StringBuilder(sc.nextLine());
+		
+		CharacterIterator it1 = new StringCharacterIterator(word1.toString());
+		while (it1.current() != CharacterIterator.DONE) {
+			Integer count = lettersMap1.containsKey(it1.current()) ? lettersMap1.get(it1.current()) : 0;
+			lettersMap1.put(it1.current(), count + 1);
+			it1.next();
+		}
+		
+		CharacterIterator it2 = new StringCharacterIterator(word2.toString());
+		while (it2.current() != CharacterIterator.DONE) {
+			lettersMap2.merge(it2.current(), 1, (k, v) -> v + 1);
+			it2.next();
+		}
+		
+		String result = checkAnagrame(lettersMap1, lettersMap2) ? "Sunt anagrame" : "Nu sunt anagrame";
+		System.out.println(result);
+		
+		sc.close();
+	}
+
+}
